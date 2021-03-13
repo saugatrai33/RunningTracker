cfc388c316b640f37e2d0a9e8f9716caa9cb8f1f
Saugat Rai <saugatrai33@gmail.com>
2021-03-13 07:52:06 +0545

    working with navigations

--- app/src/main/java/com/saugatrai/runningtracker/ui/MainActivity.kt
+++ app/src/main/java/com/saugatrai/runningtracker/ui/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.saugatrai.runningtracker.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.saugatrai.runningtracker.R
-import com.saugatrai.runningtracker.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var runDAO: RunDAO
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when (destination.id) {
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.GONE
+                }
+            }
+
     }
 }
